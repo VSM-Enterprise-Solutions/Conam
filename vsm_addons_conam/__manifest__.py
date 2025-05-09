7cc9e56960f8ca2c32c81ab61e78f1f331d70311
Matthwhy <marquesmathw@gmail.com>
2025-05-09 14:05:09 -0300

    Corrigindo: adicionando módulo corretamente como pasta

---
+++ vsm_addons_conam/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'VSM Addons Conam',
+    'version': '1.0',
+    'summary': 'Módulo de Ajustes da conam',
+    'description': """
+        Este módulo é responsável pelos ajustes em crm e vendas.
+    """,
+    'author': 'VSM Enterprise Solutions',
+    'depends': ['base','crm'],
+    'static': [
+        'static/description/icon.png', 
+    ],
+    'data': ['views/crm_lead_meeting_inherit.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
