23e5a49c268a728a398f076b00a0a3a35bc17919
Matthwhy <marquesmathw@gmail.com>
2025-05-23 13:14:23 -0300

    Atualizando manifest

--- vsm_addons_conam/__manifest__.py
+++ vsm_addons_conam/__manifest__.py
@@ -11,9 +11,20 @@
         'static/description/icon.png', 
     ],
     'data': [
+        'security/ir.model.access.csv',
+        'security/spe_record_rule.xml',
         'views/crm_lead_meeting_inherit.xml',
-        'views/sale_order_inherit.xml'
+        'views/sale_order_inherit.xml',
+        'views/crm_lead_quick_create.xml',
+        'views/crm_lead_form_inherit.xml',
+        'views/sale_order_inherit_spe.xml'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'vsm_addons_conam/static/src/css/crm_lead_custom.css',
+            'vsm_addons_conam/static/src/js/cnpj_mask.js',
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
